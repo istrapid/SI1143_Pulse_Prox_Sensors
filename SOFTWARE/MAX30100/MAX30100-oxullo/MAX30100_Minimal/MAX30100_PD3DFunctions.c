6a4677758792dbe665837ef5110abe37b21caeb1
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-02-16 11:40:44 -0500

    Began developing some functions that will be used to process/manipulate data from the pulse oximeter sensor.

---
+++ SOFTWARE/MAX30100/MAX30100-oxullo/MAX30100_Minimal/MAX30100_PD3DFunctions.c
@@ -0,0 +1,14 @@
+/*
+Additional Functions for the MAX30100 Pulse Oximeter
+
+The following script contains additional, custom functions designed to adapt the operation of oxullo's "minimal" script to our needs
+
+Fluvio L Lobo Fenoglietto 02/16/2017
+*/
+
+unint8_t averageHeartRate(int counter)
+{
+	
+	unint8_t myunintArray[counter] 
+	
+} // End of averageHeartRate()
