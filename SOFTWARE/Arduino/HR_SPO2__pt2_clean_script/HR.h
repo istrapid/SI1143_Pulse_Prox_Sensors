0dce4843acc529b12cf120bd30312383fd4fe5fa
Moe Odeh <m.odeh93@gmail.com>
2018-06-11 13:02:44 -0400

    Append missing header files

---
+++ SOFTWARE/Arduino/HR_SPO2__pt2_clean_script/HR.h
@@ -0,0 +1,7 @@
+const byte RATE_SIZE = 10; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+
